ccd23f1520f51b79c02b5ef53fa726f5edca8a2f
Jonathan Elliott <jelliot@peoplenetonline.com>
2016-07-13 14:42:21 -0500

    Splash Screen and Code Cleanup

---
+++ app/src/main/java/com/example/jelliott/idealapplication/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.jelliott.idealapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jelliott on 7/12/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash_screen_layout);
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
+
+
+
